a9ccb3cb5658f7c41d776c57c7e2f2ae10bfde95
Leonardo Alves <freitas.leoalves@gmail.com>
2023-03-13 00:38:46 -0300

    Adicionado versão baseada em recursos existentes

---
+++ v1/modules/storage-account/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  default = ""
+}
+
+variable "location" {
+  default = ""
+}
+
+variable "virtual_network_subnet_ids" {
+  default = ""
+}
+
+variable "prefix" {
+  default = ""
+}
